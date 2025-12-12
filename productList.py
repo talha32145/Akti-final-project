a717357e60d4504ff46130168a8bc77b00e5abfa
Muhammad Talha <tk579381@gmail.com>
2025-12-12 07:32:22 +0500

    Product list

---
+++ productList.py
@@ -0,0 +1,15 @@
+product_list = [
+    "Electronics", "Home Appliances", "Accessories", "Gadgets",
+    "Mobile Phones", "Smartphones",
+
+    # LG Products
+    "LG Refrigerators", "LG Washing Machines", "LG Microwave Ovens",
+    "LG Air Conditioners", "LG Televisions", "LG Irons",
+
+    # Laptops
+    "Laptops: HP, Dell, Asus ROG, Lenovo, MSI, Acer",
+    "Gaming Laptops: Asus ROG, Lenovo, MSI, Acer",
+
+    # Mobile Brands
+    "Samsung Mobiles", "Apple Mobiles", "Xiaomi Mobiles"
+]
